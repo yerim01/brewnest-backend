c3b05ccccb740e570ec5a1ca9f8a9b1599eeddcf
Yerim Moon <myr9901@gmail.com>
2025-08-01 13:43:50 -0700

    feat: added urls for category and variant.

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from store.views.product import ProductViewSet
+from store.views.product import ProductViewSet, ProductVariantViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
+router.register(r'variants', ProductVariantViewSet, basename="variant")
+router.register(r'categories', CategoryViewSet, basename="category")
 
 urlpatterns = router.urls
